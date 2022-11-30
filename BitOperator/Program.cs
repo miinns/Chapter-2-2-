db9d0ac977102b9be342f5a3daa036416e1cc83b
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 01:16:03 +0900

    Update Program.cs

--- BitOperator/Program.cs
+++ BitOperator/Program.cs
@@ -2,7 +2,7 @@
 class BitOperatorApp
 {
     public static void Main()
-    {
+   {
         int x = 9, y = 3;
         Console.WriteLine(x + " & " + y + " = " + (x & y));
         Console.WriteLine(x + " | " + y + " = " + (x | y));
